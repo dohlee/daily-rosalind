d41216028c99bb831df1945335a7b1056b490c6f
dohlee <apap950419@gmail.com>
2017-09-21 00:27:24 +0900

    Rabbits and Recurrence Relations

--- scripts/bioinformatics-stronghold/FIB.py
+++ scripts/bioinformatics-stronghold/FIB.py
@@ -19,17 +19,19 @@
 
 # Your codes here
 
-
-
-
-
-
 if __name__ == '__main__':
     # Load the data.
     with open('../../datasets/rosalind_FIB.txt') as inFile:
-        pass
+        n, k = map(int, inFile.readline().split())
+        # a represents the number of newborn rabbits.
+        # b represents the number of reproduction-age rabiits.
+        a, b = [1], [0]
+
+        for _ in range(n-1):
+            a.append(k * b[-1])
+            b.append(b[-1] + a[-2])
 
     # Print output
     with open('../../answers/rosalind_FIB_out.txt', 'w') as outFile:
-        pass
+        print(a[-1] + b[-1], file=outFile)
 
