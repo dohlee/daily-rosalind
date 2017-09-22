4aad0af6f9e6ae370cf33c87b1b48e7c01cb69a2
dohlee <apap950419@gmail.com>
2017-09-22 20:40:35 +0900

    Computing GC Content

--- scripts/bioinformatics-stronghold/GC.py
+++ scripts/bioinformatics-stronghold/GC.py
@@ -20,18 +20,39 @@
 
 
 # Your codes here
+def Fasta(filePath):
+    """Read fasta file and generate header and seq, one by one."""
+    with open(filePath) as inFile:
+        # read in the first sequence.
+        header, seqs = inFile.readline()[1:].strip(), []
 
+        for line in inFile.readlines():
+            # if new header appears, yield header and sequence.
+            if line.startswith('>'):
+                yield header, ''.join(seqs)
+                header, seqs = line[1:].strip(), []
+            else:
+                seqs.append(line.strip())
 
+        # do not forget to yield the last sequence.
+        yield header, ''.join(seqs)
 
-
-
+def gc_content(seq):
+    """Compute GC content of sequence."""
+    from collections import Counter
+    c = Counter(seq)
+    return (c['G'] + c['C']) / float(sum(c.values()))
 
 if __name__ == '__main__':
-    # Load the data.
-    with open('../../datasets/rosalind_GC.txt') as inFile:
-        pass
+    # get the sequence with the highest GC content.
+    highestGC, answer = 0, None
+    for header, seq in Fasta('../../datasets/rosalind_GC.txt'):
+        gc = gc_content(seq)
+        if gc > highestGC:
+            highestGC, answer = gc, header
 
     # Print output
     with open('../../answers/rosalind_GC_out.txt', 'w') as outFile:
-        pass
+        print(answer, file=outFile)
+        print('%.6f' % (100 * gc), file=outFile)
 
