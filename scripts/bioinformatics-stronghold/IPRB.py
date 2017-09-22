ca2543e7d1d2bea0e8313a0a8a87c5fd588c1585
dohlee <apap950419@gmail.com>
2017-09-22 20:56:55 +0900

    Mendel's First Law

--- scripts/bioinformatics-stronghold/IPRB.py
+++ scripts/bioinformatics-stronghold/IPRB.py
@@ -21,18 +21,24 @@
 
 
 # Your codes here
-
-
-
-
-
+def dominant_probability(k, m, n):
+	"""Return the probability of producing an 
+	individual with dominant phenotype.
+	"""
+	total = float(k + m + n)
+	# compute the probability of homozygous recessive.
+	pRecessiveHomozygote = (n / total) * ((n-1) / (total-1)) + \
+							2 * (m / total) * (n / (total-1)) * 0.5 + \
+							(m / total) * ((m-1) / (total-1))  * 0.25
+	# and subtract it from 1.
+	return 1 - pRecessiveHomozygote
 
 if __name__ == '__main__':
     # Load the data.
     with open('../../datasets/rosalind_IPRB.txt') as inFile:
-        pass
+        k, m, n = map(int, inFile.readline().split())
 
     # Print output
     with open('../../answers/rosalind_IPRB_out.txt', 'w') as outFile:
-        pass
+        print('%.5f' % dominant_probability(k, m, n), file=outFile)
 
