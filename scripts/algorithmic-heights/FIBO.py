4bf939790503283aaf08a95b8e52926676f42061
dohlee <apap950419@gmail.com>
2017-09-23 23:59:25 +0900

    Fibonacci Numbers

--- scripts/algorithmic-heights/FIBO.py
+++ scripts/algorithmic-heights/FIBO.py
@@ -14,18 +14,25 @@
 
 
 # Your codes here
+def fib(n):
+	"""Return nth fibonacci number."""
+	if n == 0:
+		return 0
+	elif n == 1:
+		return 1
 
+	a, b = 0, 1
+	for _ in range(n-1):
+		a, b = b, a + b
 
-
-
-
+	return b
 
 if __name__ == '__main__':
     # Load the data.
     with open('../../datasets/rosalind_FIBO.txt') as inFile:
-        pass
+        n = int(inFile.readline())
 
     # Print output
     with open('../../answers/rosalind_FIBO_out.txt', 'w') as outFile:
-        pass
+        print(fib(n), file=outFile)
 
