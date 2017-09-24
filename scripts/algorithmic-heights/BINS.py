1ce6d375947573cc825bd2bbffa9ff2fb7811150
dohlee <apap950419@gmail.com>
2017-09-24 19:23:01 +0900

    Binary Search

--- scripts/algorithmic-heights/BINS.py
+++ scripts/algorithmic-heights/BINS.py
@@ -18,18 +18,41 @@
 
 
 # Your codes here
+def binary_search(arr, k):
+	"""If k is in arr, return the index of k in arr, otherwise return -1."""
+	return binary_search_helper(arr, k, 0, len(arr))
 
+def binary_search_helper(arr, k, n, m):
+	"""Helper function for binary_search"""
+	# if no element is left in the range, return -1.
+	if n >= m:
+		return -1
 
+	# if only one element is left,
+	elif n == m-1:
+		# if the element is k, return the index (1-based).
+		if arr[n] == k:
+			return n+1
+		# else return -1.
+		else:
+			return -1
 
-
-
+	# if two or more elements are left, set midpoint and call the function recursively.
+	mid = (n + m) // 2 
+	if arr[mid] > k:
+		return binary_search_helper(arr, k, n, mid)
+	else:
+		return binary_search_helper(arr, k, mid, m)
 
 if __name__ == '__main__':
     # Load the data.
     with open('../../datasets/rosalind_BINS.txt') as inFile:
-        pass
+        n = int(inFile.readline())
+        m = int(inFile.readline())
+        arr = list(map(int, inFile.readline().split()))
+        ks = list(map(int, inFile.readline().split()))
 
     # Print output
     with open('../../answers/rosalind_BINS_out.txt', 'w') as outFile:
-        pass
+        print(' '.join(map(str, [binary_search(arr, k) for k in ks])), file=outFile)
 
