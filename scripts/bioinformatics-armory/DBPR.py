85d8286e551967cc5413408754b021a2dd3ccabc
dohlee <apap950419@gmail.com>
2017-09-26 19:34:01 +0900

    Introduction to Proetin Databases

--- scripts/bioinformatics-armory/DBPR.py
+++ scripts/bioinformatics-armory/DBPR.py
@@ -14,21 +14,23 @@
 ##################################################
 
 # Your imports here
-
+from Bio import ExPASy
+from Bio import SwissProt
 
 # Your codes here
-
-
-
-
-
+def extract_go_biological_process(accession):
+    """Get GO Biological Process term of protein via SwissProt DB."""
+    handle = ExPASy.get_sprot_raw(accession)
+    record = SwissProt.read(handle)
+    # the snippet below extracts GO Biological Process term from SwissProt record.
+    return [tup[2][2:] for tup in record.cross_references if tup[0] == 'GO' and tup[2].startswith('P')]
 
 if __name__ == '__main__':
     # Load the data.
     with open('../../datasets/rosalind_DBPR.txt') as inFile:
-        pass
+        accession = inFile.readline().strip()
 
     # Print output
     with open('../../answers/rosalind_DBPR_out.txt', 'w') as outFile:
-        pass
+        print('\n'.join(extract_go_biological_process(accession)), file=outFile)
 
