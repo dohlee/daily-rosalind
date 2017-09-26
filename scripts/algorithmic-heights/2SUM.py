0e776cf4517e5dc68116976278b26b10b8af2dce
dohlee <apap950419@gmail.com>
2017-09-26 18:58:03 +0900

    2SUM

--- scripts/algorithmic-heights/2SUM.py
+++ scripts/algorithmic-heights/2SUM.py
@@ -18,18 +18,34 @@
 
 
 # Your codes here
+def two_sum(arr):
+	"""Return a pair of numbers in array which sums to 0 if exists,
+	otherwise return a pair of -1's.
+	"""
+	negated = set(-i for i in arr)
+	for i, value in enumerate(arr):
+		if value in negated:
+			try:
+				j = arr.index(-value, i+1)
+			except ValueError:
+				continue
 
+			return (i+1, j+1)
 
-
-
-
+	return (-1, -1)
 
 if __name__ == '__main__':
     # Load the data.
     with open('../../datasets/rosalind_2SUM.txt') as inFile:
-        pass
+        k, n = map(int, inFile.readline().split())
+        arrays = [list(map(int, line.split())) for line in inFile.readlines()]
 
     # Print output
     with open('../../answers/rosalind_2SUM_out.txt', 'w') as outFile:
-        pass
+        for array in arrays:
+        	p, q = two_sum(array)
+        	if p == -1:
+        		print(-1, file=outFile)
+        	else:
+        		print(p, q, file=outFile)
 
