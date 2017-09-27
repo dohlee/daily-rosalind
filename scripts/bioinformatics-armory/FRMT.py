f91bab5f283baad9b1ce59190972cd3c14dd7e3f
dohlee <apap950419@gmail.com>
2017-09-27 16:48:58 +0900

    Data Formats

--- scripts/bioinformatics-armory/FRMT.py
+++ scripts/bioinformatics-armory/FRMT.py
@@ -13,21 +13,24 @@
 ##################################################
 
 # Your imports here
-
+from Bio import Entrez
+from Bio import SeqIO
+Entrez.email = 'apap950419@gmail.com'
 
 # Your codes here
-
-
-
-
-
+def get_genbank_fasta(ids):
+    handle = Entrez.efetch(db='nucleotide', id=ids, rettype='fasta')
+    records = list(SeqIO.parse(handle, format='fasta'))
+    return records
 
 if __name__ == '__main__':
     # Load the data.
     with open('../../datasets/rosalind_FRMT.txt') as inFile:
-        pass
+        ids = inFile.readline().split()
+        records = get_genbank_fasta(ids)
 
     # Print output
     with open('../../answers/rosalind_FRMT_out.txt', 'w') as outFile:
-        pass
-
+        shortest = min(records, key=lambda x: len(x.seq))
+        print('>' + shortest.description, file=outFile)
+        print('\n'.join(str(shortest.seq[i:i+70]) for i in range(0, len(shortest.seq), 70)), file=outFile)
