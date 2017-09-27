64ac18a219cf3b647583b6c717fe75eab85ac40a
dohlee <apap950419@gmail.com>
2017-09-27 16:32:36 +0900

    GenBank Introduction

--- scripts/bioinformatics-armory/GBK.py
+++ scripts/bioinformatics-armory/GBK.py
@@ -14,21 +14,26 @@
 ##################################################
 
 # Your imports here
-
+from Bio import Entrez
+Entrez.email = 'apap950419@gmail.com'
 
 # Your codes here
-
-
-
-
-
+def get_genbank_nucleotide_records(query):
+    """Return genbank nucleotide records given query."""
+    handle = Entrez.esearch(db='nucleotide', term=query)
+    records = Entrez.read(handle)
+    return records
 
 if __name__ == '__main__':
     # Load the data.
     with open('../../datasets/rosalind_GBK.txt') as inFile:
-        pass
+        genus = inFile.readline().strip()
+        startDate = inFile.readline().strip()
+        endDate = inFile.readline().strip()
+
+        # PDAT is search field term for publication date
+        query = '%s[Organism] AND %s:%s[PDAT]' % (genus, startDate, endDate)
 
     # Print output
     with open('../../answers/rosalind_GBK_out.txt', 'w') as outFile:
-        pass
-
+        print(get_genbank_nucleotide_records(query)['Count'], file=outFile)
