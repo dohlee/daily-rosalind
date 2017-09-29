909b897d71b5dd4bc319171961826a139ce15e69
dohlee <apap950419@gmail.com>
2017-09-30 01:13:34 +0900

    Find the Most Frequent Words in a String

--- scripts/bioinformatics-textbook-track/BA1B.py
+++ scripts/bioinformatics-textbook-track/BA1B.py
@@ -12,21 +12,22 @@
 ##################################################
 
 # Your imports here
-
+from collections import Counter
 
 # Your codes here
-
-
-
-
-
+def most_frequent_kmer(seq, k):
+	"""Return all most frequent k-mers in seq."""
+	count = Counter([seq[i:i+k] for i in range(len(seq) - k + 1)])
+	mostFrequentCount = max(count.values())
+	return [kmer for kmer in count if count[kmer] == mostFrequentCount]	
 
 if __name__ == '__main__':
     # Load the data.
     with open('../../datasets/rosalind_BA1B.txt') as inFile:
-        pass
+        seq = inFile.readline().strip()
+        k = int(inFile.readline())
 
     # Print output
     with open('../../answers/rosalind_BA1B_out.txt', 'w') as outFile:
-        pass
+        print(' '.join(sorted(most_frequent_kmer(seq, k=k))), file=outFile)
 
