1d7cde0888c5afd31f441b5355fadb6063e5c25d
dohlee <apap950419@gmail.com>
2017-10-01 14:12:35 +0900

    Mortal Fibonacci Rabbits

--- scripts/bioinformatics-stronghold/FIBD.py
+++ scripts/bioinformatics-stronghold/FIBD.py
@@ -16,18 +16,26 @@
 
 
 # Your codes here
+def fibd(n, m):
+	"""Return the total number of rabbits that will remain
+	after the n-th month if all rabbits live for m months.
+	"""
+	mat = [[0] * m for _ in range(n)]
+	mat[0][0] = 1
 
+	for month in range(1, n):
+		mat[month][0] = sum(mat[month-1][1:])
+		for j in range(m-1):
+			mat[month][j+1] = mat[month-1][j]	
 
-
-
-
+	return sum(mat[n-1])
 
 if __name__ == '__main__':
     # Load the data.
     with open('../../datasets/rosalind_FIBD.txt') as inFile:
-        pass
+    	n, m = map(int, inFile.readline().split())
 
     # Print output
     with open('../../answers/rosalind_FIBD_out.txt', 'w') as outFile:
-        pass
+        print(fibd(n, m), file=outFile)
 
