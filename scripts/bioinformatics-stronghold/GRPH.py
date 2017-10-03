d14990a4426e5ea6ed5a48fa25c50ea7b7cd7dd9
dohlee <apap950419@gmail.com>
2017-10-03 20:24:17 +0900

    Overlap Graphs

--- scripts/bioinformatics-stronghold/GRPH.py
+++ scripts/bioinformatics-stronghold/GRPH.py
@@ -13,21 +13,31 @@
 ##################################################
 
 # Your imports here
-
+from GC import Fasta
+from collections import defaultdict
 
 # Your codes here
+def overlap_graph(filePath, k=3):
+    prefix = defaultdict(list)
+    suffix = defaultdict(list)
 
+    for header, seq in Fasta(filePath):
+        prefix[seq[:k]].append(header)
+        suffix[seq[-k:]].append(header)
 
+    overlapGraph = []
+    for kmer in prefix:
+        for h in prefix[kmer]:
+            for s in suffix[kmer]:
+                if h != s:
+                    overlapGraph.append((s, h))
 
-
-
+    return overlapGraph
 
 if __name__ == '__main__':
     # Load the data.
-    with open('../../datasets/rosalind_GRPH.txt') as inFile:
-        pass
+    overlapGraph = overlap_graph('../../datasets/rosalind_GRPH.txt')        
 
     # Print output
     with open('../../answers/rosalind_GRPH_out.txt', 'w') as outFile:
-        pass
-
+        print('\n'.join([' '.join([src, dst]) for src, dst in overlapGraph]), file=outFile)
