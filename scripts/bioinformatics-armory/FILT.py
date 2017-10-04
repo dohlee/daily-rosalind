53402a77a30804c2d4c90bb85343d3a052bfa3a7
dohlee <apap950419@gmail.com>
2017-10-04 18:25:11 +0900

    Read Filtration by Quality

--- scripts/bioinformatics-armory/FILT.py
+++ scripts/bioinformatics-armory/FILT.py
@@ -12,21 +12,25 @@
 ##################################################
 
 # Your imports here
-
+from PHRE import Fastq_fp
 
 # Your codes here
-
-
-
-
-
+def filter(qualities, threshold, percentage):
+	"""Return True if more than percentage(%) of bases
+	have better or equal quality than specified threshold.
+	"""
+	return sum((ord(c) - 33) >= threshold for c in qualities) / len(qualities) >= percentage / 100
 
 if __name__ == '__main__':
     # Load the data.
     with open('../../datasets/rosalind_FILT.txt') as inFile:
-        pass
+        threshold, percentage = map(int, inFile.readline().split())
+        c = 0
+        for header, seq, comment, quality in Fastq_fp(inFile):
+        	if filter(quality, threshold, percentage):
+        		c += 1
 
     # Print output
     with open('../../answers/rosalind_FILT_out.txt', 'w') as outFile:
-        pass
+        print(c, file=outFile)
 
