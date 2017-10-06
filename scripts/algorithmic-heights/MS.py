53093187938259c53f60c01aaea1881085644f1c
dohlee <apap950419@gmail.com>
2017-10-06 22:58:06 +0900

    Merge Sort

--- scripts/algorithmic-heights/MS.py
+++ scripts/algorithmic-heights/MS.py
@@ -12,21 +12,27 @@
 ##################################################
 
 # Your imports here
-
+from MER import merge
 
 # Your codes here
+def merge_sort(arr):
+    """Mergesort arr."""
+    if len(arr) == 1:
+        return arr
 
+    mid = len(arr) // 2
+    arr1 = merge_sort(arr[:mid])
+    arr2 = merge_sort(arr[mid:])
 
-
-
-
+    return merge(arr1, arr2)
 
 if __name__ == '__main__':
     # Load the data.
     with open('../../datasets/rosalind_MS.txt') as inFile:
-        pass
+        inFile.readline()
+        arr = list(map(int, inFile.readline().split()))
 
     # Print output
     with open('../../answers/rosalind_MS_out.txt', 'w') as outFile:
-        pass
+        print(' '.join(map(str, merge_sort(arr))), file=outFile)
 
