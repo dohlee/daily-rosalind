9ddb55e3a6859645fbe4761e6e9c49ae9897a0e8
dohlee <apap950419@gmail.com>
2017-10-08 23:28:30 +0900

    RNA splicing

--- scripts/bioinformatics-stronghold/SPLC.py
+++ scripts/bioinformatics-stronghold/SPLC.py
@@ -15,21 +15,31 @@
 ##################################################
 
 # Your imports here
-
+from PROT import translate
+from GC import Fasta
 
 # Your codes here
+def splice_translate(seq, introns):
+    """Translate sequence after splicing introns out."""
+    # map each intron to the sequence to get their coordinates.
+    intronCoordinates = sorted([(seq.index(intron), seq.index(intron) + len(intron)) for intron in introns], \
+                                key=lambda x: x[0])
+    intronCoordinates = [(0, 0)] + intronCoordinates + [(len(seq), len(seq))]
 
+    # from coordinates of introns, get coordinates of exons.
+    exonCoordinates = [(x[1], y[0]) for x, y in zip(intronCoordinates[:-1], intronCoordinates[1:])]
 
-
-
-
+    # convert sequence into rna and return translated amino acid sequence.
+    rna = ''.join([seq[start:end] for start, end in exonCoordinates]).replace('T', 'U')
+    return translate(rna)
 
 if __name__ == '__main__':
     # Load the data.
-    with open('../../datasets/rosalind_SPLC.txt') as inFile:
-        pass
+    seqs = [seq for header, seq in Fasta('../../datasets/rosalind_SPLC.txt')]
+    seq = seqs[0]
+    introns = seqs[1:]
 
     # Print output
     with open('../../answers/rosalind_SPLC_out.txt', 'w') as outFile:
-        pass
+        print(splice_translate(seq, introns), file=outFile)
 
