edaafcb21785fb519db3de6f58f6195b95841b69
dohlee <apap950419@gmail.com>
2017-10-10 23:38:19 +0900

    Enumerating Gene Orders

--- scripts/bioinformatics-stronghold/PERM.py
+++ scripts/bioinformatics-stronghold/PERM.py
@@ -16,18 +16,30 @@
 
 
 # Your codes here
+def permutation_helper(lst):
+    """Helper function for recursive call."""
+    if len(lst) == 1:
+        return [lst]
 
+    p = []
+    for i in range(len(lst)):
+        p.extend([[lst[i]] + perm for perm in permutation_helper(lst[:i] + lst[i+1:])])
+    return p
 
-
-
-
+def permutation(l):
+    """Return the list of all permutations of length n."""
+    lst = [x for x in range(1, l+1)]
+    return permutation_helper(lst)
 
 if __name__ == '__main__':
     # Load the data.
     with open('../../datasets/rosalind_PERM.txt') as inFile:
-        pass
+        l = int(inFile.readline())
 
     # Print output
     with open('../../answers/rosalind_PERM_out.txt', 'w') as outFile:
-        pass
+        perm = permutation(l)
+        print(len(perm), file=outFile)
+        for p in perm:
+            print(' '.join(map(str, p)), file=outFile)
 
