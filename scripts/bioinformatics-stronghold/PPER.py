5413d43e9538267ab75942a4ff5824ac8cbc438b
dohlee <apap950419@gmail.com>
2017-10-12 23:50:34 +0900

    Partial Permutations

--- scripts/bioinformatics-stronghold/PPER.py
+++ scripts/bioinformatics-stronghold/PPER.py
@@ -13,21 +13,24 @@
 ##################################################
 
 # Your imports here
-
+from functools import reduce
+import operator
 
 # Your codes here
-
-
-
-
-
+def prod(iterable):
+	"""Return the product of elements in interable."""
+	return reduce(operator.mul, iterable, 1)
+	
+def partial_permutation(n, r):
+	"""Return nPr."""
+	return prod(range(n, n-r, -1))
 
 if __name__ == '__main__':
     # Load the data.
     with open('../../datasets/rosalind_PPER.txt') as inFile:
-        pass
+        n, r = map(int, inFile.readline().split())
 
     # Print output
     with open('../../answers/rosalind_PPER_out.txt', 'w') as outFile:
-        pass
+        print(partial_permutation(n, r) % 1000000, file=outFile)
 
