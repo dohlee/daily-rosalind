afe507d28118f4820646f2a511cd7159581170d4
dohlee <apap950419@gmail.com>
2017-10-19 21:45:13 +0900

    Perfect Matchings and RNA Secondary Structures

--- scripts/bioinformatics-stronghold/PMCH.py
+++ scripts/bioinformatics-stronghold/PMCH.py
@@ -15,21 +15,24 @@
 ##################################################
 
 # Your imports here
-
+from GC import Fasta
 
 # Your codes here
+def factorial(n):
+	"""Return n!."""
+	if n == 1:
+		return 1
+	return n * factorial(n-1)
 
-
-
-
-
+def count_perfect_matchings(seq):
+	"""Return the number of perfect matchings in the sequence."""
+	return factorial(seq.count('A')) * factorial(seq.count('G'))
 
 if __name__ == '__main__':
     # Load the data.
-    with open('../../datasets/rosalind_PMCH.txt') as inFile:
-        pass
+    seq = [seq for header, seq in Fasta('../../datasets/rosalind_PMCH.txt')][0]
 
     # Print output
     with open('../../answers/rosalind_PMCH_out.txt', 'w') as outFile:
-        pass
+        print(count_perfect_matchings(seq), file=outFile)
 
