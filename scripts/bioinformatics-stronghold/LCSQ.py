53323a34f8150458b7edd0f955f46e405f7f2d13
dohlee <apap950419@gmail.com>
2017-10-23 17:27:21 +0900

    Finding a Shared Spliced Motif

--- scripts/bioinformatics-stronghold/LCSQ.py
+++ scripts/bioinformatics-stronghold/LCSQ.py
@@ -14,21 +14,28 @@
 ##################################################
 
 # Your imports here
-
+from GC import Fasta
+from GLOB import DPMatrix
 
 # Your codes here
+def longest_common_subsequence(seq1, seq2):
+    """Return the longest common subsequence of two sequences."""
+    mat = DPMatrix(seq1, seq2, match=1, mismatch=0, gap=0)
+    mat.fill()
 
+    lcs = []
+    augSeq1, augSeq2 = mat.augmented_sequences()
+    for c1, c2 in zip(augSeq1, augSeq2):
+        if c1 == c2:
+            lcs.append(c1)
 
-
-
-
+    return ''.join(lcs)
 
 if __name__ == '__main__':
     # Load the data.
-    with open('../../datasets/rosalind_LCSQ.txt') as inFile:
-        pass
+    seq1, seq2 = [seq for header, seq in Fasta('../../datasets/rosalind_LCSQ.txt')]
 
     # Print output
     with open('../../answers/rosalind_LCSQ_out.txt', 'w') as outFile:
-        pass
+        print(longest_common_subsequence(seq1, seq2), file=outFile)
 
