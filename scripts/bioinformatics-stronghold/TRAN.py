fd8f0370f17513d06015e26924c266990959bd51
dohlee <apap950419@gmail.com>
2017-10-23 15:06:27 +0900

    Transitions and Transversions

--- scripts/bioinformatics-stronghold/TRAN.py
+++ scripts/bioinformatics-stronghold/TRAN.py
@@ -13,21 +13,36 @@
 ##################################################
 
 # Your imports here
-
+from GC import Fasta
 
 # Your codes here
+def is_transition(base1, base2):
+    """Return whether substitution between base1 and base2 is transition."""
+    purines = {'A', 'G'}
+    pyrimidines = {'C', 'T'}
 
-
-
-
-
+    if (base1 in purines and base2 in purines) or (base1 in pyrimidines and base2 in pyrimidines):
+        return True
+    else:
+        return False
+def transition_transversion_ratio(seq1, seq2):
+    """Given two DNA sequences of equal length,
+    return the transition(G<->A, C<->T)/transversion(else) ratio.
+    """
+    transitionCount, transversionCount = 0.0, 0.0
+    for base1, base2 in zip(seq1, seq2):
+        if base1 != base2:
+            if is_transition(base1, base2):
+                transitionCount += 1
+            else:
+                transversionCount += 1
+    return transitionCount / transversionCount
 
 if __name__ == '__main__':
     # Load the data.
-    with open('../../datasets/rosalind_TRAN.txt') as inFile:
-        pass
+    seq1, seq2 = [seq for header, seq in Fasta('../../datasets/rosalind_TRAN.txt')]
 
     # Print output
     with open('../../answers/rosalind_TRAN_out.txt', 'w') as outFile:
-        pass
+        print('%.11f' % transition_transversion_ratio(seq1, seq2), file=outFile)
 
