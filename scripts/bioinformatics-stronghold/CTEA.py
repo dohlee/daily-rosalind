8fb5eb6ed415a2c81d92f985c9a743737a337d3c
dohlee <apap950419@gmail.com>
2017-10-23 17:04:30 +0900

    Counting Optimal Alignments

--- scripts/bioinformatics-stronghold/CTEA.py
+++ scripts/bioinformatics-stronghold/CTEA.py
@@ -14,21 +14,42 @@
 ##################################################
 
 # Your imports here
-
+from GC import Fasta
+from GLOB import DPMatrix
 
 # Your codes here
+def initialize(self):
+    """Initializer for counting optimal alignments."""
+    for i in range(len(self.seq1) + 1):
+        self.mat[i][0] = (self.gap * i, 1)
+        self.backtrackMat[i][0] = self.DONE  # We don't need backtracking
+    for j in range(len(self.seq2) + 1):
+        self.mat[0][j] = (self.gap * j, 1)
+        self.backtrackMat[0][j] = self.DONE  # We don't need backtracking
 
+def recurrence_relation(i, j, self):
+    """Recurrence relation for counting optimal alignments."""
+    candidates = [self.mat[i-1][j-1][0] + [self.mismatch, self.match][self.seq1[i-1] == self.seq2[j-1]],
+                    self.mat[i-1][j][0] + self.gap,
+                    self.mat[i][j-1][0] + self.gap]
+    alignmentCounts = [self.mat[i-1][j-1][1], self.mat[i-1][j][1], self.mat[i][j-1][1]]
 
+    alignmentCount = 0
+    minScore = min(candidates)
+    for i in range(3):
+        if candidates[i] == minScore:
+            alignmentCount += alignmentCounts[i]
 
-
-
+    return (self.DONE, (minScore, alignmentCount))
 
 if __name__ == '__main__':
     # Load the data.
-    with open('../../datasets/rosalind_CTEA.txt') as inFile:
-        pass
-
+    seq1, seq2 = [seq for header, seq in Fasta('../../datasets/rosalind_CTEA.txt')]
+    mat = DPMatrix(seq1, seq2, match=0, mismatch=1, gap=1)
+    mat.set_initializer(initialize)
+    mat.set_recurrence_relation(recurrence_relation)
+    mat.fill()
     # Print output
     with open('../../answers/rosalind_CTEA_out.txt', 'w') as outFile:
-        pass
+        print(mat[len(seq1)][len(seq2)][1] % 134217727, file=outFile)
 
