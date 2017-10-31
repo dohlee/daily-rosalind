16f42b13fa43cd0d82636b4836e8bdae2b09dcbb
dohlee <apap950419@gmail.com>
2017-10-31 14:48:57 +0900

    Implement NumberToPattern

--- scripts/bioinformatics-textbook-track/BA1M.py
+++ scripts/bioinformatics-textbook-track/BA1M.py
@@ -14,18 +14,24 @@
 
 
 # Your codes here
-
-
-
-
-
+def num2pattern(index, k):
+    """Convert an integer into a dna string of length k."""
+    d = dict(zip('0123', 'ACGT'))
+    c = []
+    while index > 4:
+        c.append(index % 4)
+        index //= 4
+    c.append(index)
+    c += [0] * (k - len(c))
+    return ''.join(map(lambda x: d[str(x)], c[::-1]))
 
 if __name__ == '__main__':
     # Load the data.
     with open('../../datasets/rosalind_BA1M.txt') as inFile:
-        pass
+        index = int(inFile.readline())
+        k = int(inFile.readline())
 
     # Print output
     with open('../../answers/rosalind_BA1M_out.txt', 'w') as outFile:
-        pass
+        print(num2pattern(index, k), file=outFile)
 
