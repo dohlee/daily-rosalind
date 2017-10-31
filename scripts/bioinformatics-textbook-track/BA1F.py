1d38b396c7e1bef4e6ca833be8c8dbc52e0514c4
dohlee <apap950419@gmail.com>
2017-10-31 15:20:08 +0900

    Find a Position in a Genome Minimizing the Skew

--- scripts/bioinformatics-textbook-track/BA1F.py
+++ scripts/bioinformatics-textbook-track/BA1F.py
@@ -15,18 +15,30 @@
 
 
 # Your codes here
+def skew_minimizing_positions(dna):
+    """Return the positions that minimize the G-C skew of dna[:pos]."""
+    minSkew, skew = 12345679, 0
+    pos = []
+    for i, base in enumerate(dna, 1):
+        if base == 'C':
+            skew -= 1
+        elif base == 'G':
+            skew += 1
 
+        if skew < minSkew:
+            minSkew = skew
+            pos = [i]
+        elif skew == minSkew:
+            pos.append(i)
 
-
-
-
+    return pos
 
 if __name__ == '__main__':
     # Load the data.
     with open('../../datasets/rosalind_BA1F.txt') as inFile:
-        pass
+        dna = inFile.readline().strip()
 
     # Print output
     with open('../../answers/rosalind_BA1F_out.txt', 'w') as outFile:
-        pass
+        print(' '.join(map(str, skew_minimizing_positions(dna))), file=outFile)
 
