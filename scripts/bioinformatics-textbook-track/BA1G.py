d9164c1f3d094997bf7e239f198be83c7e141297
dohlee <apap950419@gmail.com>
2017-10-31 14:58:39 +0900

    Compute the Hamming Distance Between Two Strings

--- scripts/bioinformatics-textbook-track/BA1G.py
+++ scripts/bioinformatics-textbook-track/BA1G.py
@@ -15,18 +15,21 @@
 
 
 # Your codes here
-
-
-
-
-
+def hamming_distance(seq1, seq2):
+    """Return hamming distance of two sequence."""
+    # note that hamming distance is defined for two sequences
+    # with the same length.
+    assert len(seq1) == len(seq2)
+    # count the number of mismatching characters in pythonic way!
+    return sum(c1 != c2 for c1, c2 in zip(seq1, seq2))
 
 if __name__ == '__main__':
     # Load the data.
     with open('../../datasets/rosalind_BA1G.txt') as inFile:
-        pass
+        seq1 = inFile.readline().strip()
+        seq2 = inFile.readline().strip()
 
     # Print output
     with open('../../answers/rosalind_BA1G_out.txt', 'w') as outFile:
-        pass
+        print(hamming_distance(seq1, seq2), file=outFile)
 
