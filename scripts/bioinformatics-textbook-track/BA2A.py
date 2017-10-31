3749ee3a32d50290370d9158b03299e47b710e64
dohlee <apap950419@gmail.com>
2017-10-31 16:06:45 +0900

    Implement MotifEnumeration

--- scripts/bioinformatics-textbook-track/BA2A.py
+++ scripts/bioinformatics-textbook-track/BA2A.py
@@ -12,21 +12,35 @@
 ##################################################
 
 # Your imports here
-
+from BA1E import enumerate_kmers
+from BA1N import neighbors
+from collections import Counter
 
 # Your codes here
+def find_motif(dnas, k, d):
+    """Return the list of (k, d)-motifs in DNAs."""
+    # Hash all occurences of k-mers (including d-neighbors) in each DNA string.
+    motifCounts = [Counter() for _ in range(len(dnas))]
+    for count, dna in zip(motifCounts, dnas):
+        for i, kmer in enumerate_kmers(dna, k):
+            for neighbor in neighbors(kmer, d):
+                count[neighbor] += 1
 
+    # Find motif that occurs in all DNAs.
+    motives = []
+    for motif in motifCounts[0]:
+        if all(count[motif] > 0 for count in motifCounts[1:]):
+            motives.append(motif)
 
-
-
-
+    return motives
 
 if __name__ == '__main__':
     # Load the data.
     with open('../../datasets/rosalind_BA2A.txt') as inFile:
-        pass
+        k, d = map(int, inFile.readline().split())
+        dnas = [line.strip() for line in inFile.readlines()]
 
     # Print output
     with open('../../answers/rosalind_BA2A_out.txt', 'w') as outFile:
-        pass
+        print(' '.join(find_motif(dnas, k, d)), file=outFile)
 
