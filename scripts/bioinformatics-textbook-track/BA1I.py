f09434a0fb2d2de8e33535804cbb6832033a3eea
dohlee <apap950419@gmail.com>
2017-10-31 15:50:48 +0900

    Find the Most Frequent Words with Mismatches in a String

--- scripts/bioinformatics-textbook-track/BA1I.py
+++ scripts/bioinformatics-textbook-track/BA1I.py
@@ -12,21 +12,29 @@
 ##################################################
 
 # Your imports here
-
+from BA1E import enumerate_kmers
+from BA1N import neighbors
+from collections import Counter
 
 # Your codes here
+def most_frequent_words_with_mismatches(string, k, d):
+    """Return the list of the most frequent k-mers when at most 
+    d mismatches are allowed.
+    """
+    counter = Counter()
+    for i, kmer in enumerate_kmers(string, k):
+        for neighbor in neighbors(kmer, d):
+            counter[neighbor] += 1
 
-
-
-
-
+    maxCount = max(counter.values())
+    return [kmer for kmer in counter if counter[kmer] == maxCount]
 
 if __name__ == '__main__':
     # Load the data.
     with open('../../datasets/rosalind_BA1I.txt') as inFile:
-        pass
+        string = inFile.readline().strip()
+        k, d = map(int, inFile.readline().split())
 
     # Print output
     with open('../../answers/rosalind_BA1I_out.txt', 'w') as outFile:
-        pass
-
+        print(' '.join(most_frequent_words_with_mismatches(string, k, d)), file=outFile)
