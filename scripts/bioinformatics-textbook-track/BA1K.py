765242e3d4d8c762339694a0b8af9b2152d6a3cc
dohlee <apap950419@gmail.com>
2017-10-31 14:55:13 +0900

    Generate the Frequency Array of a String

--- scripts/bioinformatics-textbook-track/BA1K.py
+++ scripts/bioinformatics-textbook-track/BA1K.py
@@ -11,21 +11,24 @@
 ##################################################
 
 # Your imports here
-
+from BA1L import pattern2num
 
 # Your codes here
+def frequency_array(dna, k):
+    """Generate frequency array of k-mers in DNA."""
+    frequencyArray = [0] * (4**k)
+    for i in range(0, len(dna) - k + 1):
+        frequencyArray[pattern2num(dna[i:i+k])] += 1
 
-
-
-
+    return frequencyArray
 
 
 if __name__ == '__main__':
     # Load the data.
     with open('../../datasets/rosalind_BA1K.txt') as inFile:
-        pass
+        dna = inFile.readline().strip()
+        k = int(inFile.readline())
 
     # Print output
     with open('../../answers/rosalind_BA1K_out.txt', 'w') as outFile:
-        pass
-
+        print(' '.join(map(str, frequency_array(dna, k))), file=outFile)
