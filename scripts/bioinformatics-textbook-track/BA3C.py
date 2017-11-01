b8e03946ba16b24e9a6e43ff149b2415de7c58fb
dohlee <apap950419@gmail.com>
2017-11-01 13:51:55 +0900

    Construct the Overlap Graph of a Collection of k-mers

--- scripts/bioinformatics-textbook-track/BA3C.py
+++ scripts/bioinformatics-textbook-track/BA3C.py
@@ -12,21 +12,37 @@
 ##################################################
 
 # Your imports here
-
+from collections import defaultdict
 
 # Your codes here
+def overlap_graph(kmers):
+    """Return the overlap graph of given k-mers."""
+    overlapGraph = defaultdict(list)
+    prefixDict = defaultdict(list)
+    suffixDict = defaultdict(list) 
+    for kmer in kmers:
+        prefixDict[kmer[:-1]].append(kmer)
+        suffixDict[kmer[1:]].append(kmer)
 
+    for suffix in suffixDict:
+        if suffix not in prefixDict:
+            continue
+        for u in suffixDict[suffix]:
+            for v in prefixDict[suffix]:
+                overlapGraph[u].append(v)
 
-
-
-
+    return overlapGraph
 
 if __name__ == '__main__':
     # Load the data.
     with open('../../datasets/rosalind_BA3C.txt') as inFile:
-        pass
+        kmers = [line.strip() for line in inFile.readlines()]
 
     # Print output
     with open('../../answers/rosalind_BA3C_out.txt', 'w') as outFile:
-        pass
+        overlapGraph = overlap_graph(kmers)
+        for u in overlapGraph:
+            for v in overlapGraph[u]:
+                print('%s -> %s' % (u, v), file=outFile)
+
 
