5fde7920ea6ccf9932c5df3fc72e56125c888ab0
dohlee <apap950419@gmail.com>
2017-11-02 20:01:43 +0900

    Character-Based Phylogeny

--- scripts/bioinformatics-stronghold/CHBP.py
+++ scripts/bioinformatics-stronghold/CHBP.py
@@ -17,18 +17,49 @@
 
 
 # Your codes here
+def merge_two_nodes(characterTable, nodeNames, i, j):
+	"""Merge two nodes."""
+	assert i < j
+	nodeNames[i] = '(%s,%s)' % (nodeNames[i], nodeNames[j])
+	nodeNames.pop(j)
 
+	for row in characterTable:
+		row.pop(j)
 
+	if len(characterTable) > 1:
+		toRemove = [i for i, row in enumerate(characterTable) if row.count('1') == 1 or row.count('0') == 1]
+		for i in toRemove:
+			del characterTable[i]
 
+def select_nodes_to_merge(characterTable):
+	"""When characters of two nodes are consistent for all rows
+	in character table, they are ready to be merged.
+	"""
+	for i in range(len(characterTable[0])):
+		for j in range(i+1, len(characterTable[0])):
+			if all(row[i] == row[j] for row in characterTable):
+				return i, j
 
+	return None, None
 
+def reconstruct_tree(characterTable, nodeNames):
+	"""Reconstruct unrooted binary tree from character table by
+	iteratively merging two nodes.
+	"""
+	i, j = select_nodes_to_merge(characterTable)
+
+	while not (i is None and j is None):
+		merge_two_nodes(characterTable, nodeNames, i, j)
+		i, j = select_nodes_to_merge(characterTable)
+
+	return '(%s)' % ','.join(nodeNames)
 
 if __name__ == '__main__':
     # Load the data.
     with open('../../datasets/rosalind_CHBP.txt') as inFile:
-        pass
+        nodeNames = inFile.readline().strip().split()
+        characterTable = [list(line.strip()) for line in inFile.readlines()]
 
     # Print output
     with open('../../answers/rosalind_CHBP_out.txt', 'w') as outFile:
-        pass
-
+        print(reconstruct_tree(characterTable, nodeNames), file=outFile)
