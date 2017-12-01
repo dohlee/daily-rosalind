f0df39c81ff5eee9f85b52ec2f25b1470e186f3e
dohlee <apap950419@gmail.com>
2017-12-01 17:35:30 +0900

    inferring Protein from Spectrum

--- scripts/bioinformatics-stronghold/SPEC.py
+++ scripts/bioinformatics-stronghold/SPEC.py
@@ -15,21 +15,35 @@
 ##################################################
 
 # Your imports here
-
+from PRTM import massTable
 
 # Your codes here
+massToAminoacid = dict((mass, aa) for aa, mass in massTable.items())
 
+def get_amino_acid_given_mass(mass):
+	minDiff = 12345679
+	for key in massToAminoacid:
+		diff = abs(mass - key)
+		if diff < minDiff:
+			minDiff = diff 
+			aa = massToAminoacid[key]
 
+	return aa
 
-
-
+def infer_protein_from_spectrum(prefixSpectrum):
+	"""Given prefix spectrum of a protein string,
+	return one of the possible protein string which can
+	generate the given prefix spectrum.
+	"""
+	masses = [b - a for a, b in zip(prefixSpectrum[:-1], prefixSpectrum[1:])]
+	return ''.join([get_amino_acid_given_mass(mass) for mass in masses])
 
 if __name__ == '__main__':
     # Load the data.
     with open('../../datasets/rosalind_SPEC.txt') as inFile:
-        pass
+        prefixSpectrum = list(map(float, inFile.readlines()))
 
     # Print output
     with open('../../answers/rosalind_SPEC_out.txt', 'w') as outFile:
-        pass
+        print(infer_protein_from_spectrum(prefixSpectrum), file=outFile)
 
