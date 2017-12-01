2f21ddbecf001f01cbe2ac967667f97046008088
dohlee <apap950419@gmail.com>
2017-12-01 18:05:16 +0900

    Matching a Spectrum to a Protein

--- scripts/bioinformatics-stronghold/PRSM.py
+++ scripts/bioinformatics-stronghold/PRSM.py
@@ -19,21 +19,44 @@
 ##################################################
 
 # Your imports here
-
+from PRTM import massTable
+from collections import Counter
 
 # Your codes here
+def weight(peptide):
+	"""Return the weight of the peptide."""
+	return sum(massTable[aa] for aa in peptide)
 
-
-
-
-
+def complete_spectrum(peptide):
+	"""Return the complete spectrum of the peptide.
+	Complete spectrum is a multiset of weights of all 
+	prefixes and suffixes occurring in peptide.
+	"""
+	spectrum = []
+	for i in range(len(peptide)):
+		spectrum.append(weight(peptide[:i]))
+		spectrum.append(weight(peptide[i:]))
+	
+	return spectrum
 
 if __name__ == '__main__':
     # Load the data.
     with open('../../datasets/rosalind_PRSM.txt') as inFile:
-        pass
+        n = int(inFile.readline())
+        peptides = [inFile.readline().strip() for _ in range(n)]
+        R = list(map(float, inFile.readlines()))
 
     # Print output
     with open('../../answers/rosalind_PRSM_out.txt', 'w') as outFile:
-        pass
+    	maxMultiplicity = 0
+    	for peptide in peptides:
+    		spectrum = complete_spectrum(peptide)
+    		minkowskiDifference = Counter([round(abs(b - a), 5) for b in R for a in spectrum])
 
+    		multiplicity = minkowskiDifference.most_common(1)[0][1]
+    		if multiplicity >= maxMultiplicity:
+    			maxMultiplicity = multiplicity
+    			answer = peptide
+
+    	print(maxMultiplicity, file=outFile)
+    	print(answer, file=outFile)
