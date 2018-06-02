62594bba485a76feddefbe13a6e56842f5c39e72
dohlee <apap950419@gmail.com>
2018-06-03 01:14:54 +0900

    Independent Segregation of Chromosomes

--- scripts/bioinformatics-stronghold/INDC.py
+++ scripts/bioinformatics-stronghold/INDC.py
@@ -2,9 +2,9 @@
 # Independent Segregation of Chromosomes
 #
 # http://rosalind.info/problems/INDC/
-# 
+#
 # Given: A positive integer n <=q 50.
-# 
+#
 # Return: An array A of length 2n in which A[k]
 #  represents the common logarithm of the probability
 #  that two diploid siblings share at least k of
@@ -15,21 +15,15 @@
 ##################################################
 
 # Your imports here
-
+from ASPC import combination
+import numpy as np
 
 # Your codes here
-
-
-
-
-
-
 if __name__ == '__main__':
     # Load the data.
-    with open('../../datasets/rosalind_INDC.txt') as inFile:
-        pass
+    n = int(open('../../datasets/rosalind_INDC.txt').read())
 
     # Print output
     with open('../../answers/rosalind_INDC_out.txt', 'w') as outFile:
-        pass
-
+        answer = np.log10(np.array([combination(2 * n, i) * (0.5)**(2 * n) for i in range(2 * n, 0, -1)]).cumsum())[::-1]
+        print(' '.join(map(lambda x: '%.4f' % x, answer)), file=outFile)
