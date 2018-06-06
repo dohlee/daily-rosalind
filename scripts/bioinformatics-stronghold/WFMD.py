7dab1f9b6ff97482483b637a3e111796a01be355
dohlee <apap950419@gmail.com>
2018-06-06 22:03:58 +0900

    The Wright-Fisher Model of Genetic Drift

--- scripts/bioinformatics-stronghold/WFMD.py
+++ scripts/bioinformatics-stronghold/WFMD.py
@@ -2,10 +2,10 @@
 # The Wright-Fisher Model of Genetic Drift
 #
 # http://rosalind.info/problems/WFMD/
-# 
+#
 # Given: Positive integers N (N <=q 7), m (m <=q
 #  2N), g (g <=q 6) and k (k <=q 2N).
-# 
+#
 # Return: The probability that in a population of
 #  N diploid individuals initially possessing m
 #  copies of a dominant allele, we will observe
@@ -16,21 +16,24 @@
 ##################################################
 
 # Your imports here
-
+from ASPC import combination
 
 # Your codes here
-
-
-
-
-
-
 if __name__ == '__main__':
     # Load the data.
     with open('../../datasets/rosalind_WFMD.txt') as inFile:
-        pass
+        N, m, g, k = map(int, inFile.readline().strip().split())
+        p = m / (2 * N)
+
+    # Probabilities of having x donimant alleles in 1st generation.
+    p_dominant_alleles = [combination(2 * N, i) * p**i * (1 - p)**(2 * N - i) for i in range(2 * N + 1)]
+    for _ in range(g - 1):
+        temp = [0] * (2 * N + 1)
+        for i in range(2 * N + 1):
+            for j, p in enumerate(p_dominant_alleles):
+                temp[i] += p * combination(2 * N, i) * (j / (2 * N))**i * (1 - (j / (2 * N)))**(2 * N - i)
+        p_dominant_alleles = temp
 
     # Print output
     with open('../../answers/rosalind_WFMD_out.txt', 'w') as outFile:
-        pass
-
+        print('%.3f' % sum(p_dominant_alleles[:-k]), file=outFile)
