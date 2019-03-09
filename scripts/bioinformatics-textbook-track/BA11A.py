824987f56689849bd13f64917140033c2df155d7
dohlee <dohlee.bioinfo@gmail.com>
2019-03-09 14:33:40 +0900

    Construct the Graph of a Spectrum

--- scripts/bioinformatics-textbook-track/BA11A.py
+++ scripts/bioinformatics-textbook-track/BA11A.py
@@ -11,21 +11,58 @@
 ##################################################
 
 # Your imports here
-
+from collections import defaultdict
 
 # Your codes here
+massTable = """
+A   71
+C   103
+D   115
+E   129
+F   147
+G   57
+H   137
+I   113
+K   128
+L   113
+M   131
+N   114
+P   97
+Q   128
+R   156
+S   87
+T   101
+V   99
+W   186
+Y   163
+X   4
+Z   5
+""".split()
+aa2mass = dict(zip(massTable[::2], map(int,massTable[1::2])))
+mass2aa = dict(zip(map(int,massTable[1::2]), massTable[::2]))
 
+def graph(spectrum):
+    spectrum = [0] + spectrum
+    l = len(spectrum)
+    adj_list = defaultdict(list)
 
-
-
-
+    for i in range(l):
+        for j in range(i+1, l):
+            diff = spectrum[j] - spectrum[i]
+            if diff in mass2aa:
+                adj_list[spectrum[i]].append((spectrum[j], mass2aa[diff]))
+    
+    return adj_list
 
 if __name__ == '__main__':
     # Load the data.
     with open('../../datasets/rosalind_BA11A.txt') as inFile:
-        pass
+        spectrum = list(map(int, inFile.readline().strip().split()))
 
+    adj_list = graph(spectrum)
     # Print output
     with open('../../answers/rosalind_BA11A_out.txt', 'w') as outFile:
-        pass
-
+        for u in sorted(adj_list.keys()):
+            vs = adj_list[u]
+            for v, aa in vs:
+                print('%d->%d:%s' % (u, v, aa), file=outFile)
